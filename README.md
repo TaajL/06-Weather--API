9eb832ad276241d8ce3c47bf037adabb8a9f3fa4
Malcom X <Taajlogan@gmail.com>
2024-07-23 11:30:30 -0400

    read me and script js

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # 06-Weather--API
+## Description 
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Deployed Application 
+[Github Repo link](https://github.com/TaajL/06-Weather--API)
+
+
+ ![screenshot](/JATE.png)
+
+## Installation 
+DAY JS
+## Usage 
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+## Contributing
+Contributions are welcome! Please submit a pull request.
+
+## License
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+## Authors
+* [Taaj Logan](https://github.com/TaajL) 
+
+## Credits 
+N/A
--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-// Global variables to be used in functions
 let userSearches = [];
 let searchButton = document.querySelector("#search");
 let recentSearchesList = document.querySelector("#recent-searches-list");
 let noSearchesMessage = document.querySelector("#no-searches-message");
 
-window.onload = function () {
+window.onload = function() {
     renderPastSearches();
-};
+}
 
-const renderPastSearches = () => {
-    const savedSearches = JSON.parse(localStorage.getItem("userSearches"));
+renderPastSearches = function() {
+    let savedSearches = JSON.parse(localStorage.getItem("userSearches"));
     if (savedSearches) {
         userSearches = savedSearches;
-        savedSearches.forEach((userSearch) => {
-            const listItem = document.createElement("li");
-            const button = document.createElement("button");
+        recentSearchesList.innerHTML = "";
+        savedSearches.forEach(userSearch => {
+            let pastSearch = document.createElement("li");
+            let button = document.createElement("button");
             button.innerHTML = userSearch;
-            button.addEventListener("click", () => {
+            button.addEventListener("click", function() {
                 displayWeather(userSearch);
             });
-            listItem.appendChild(button);
-            recentSearchesList.appendChild(listItem);
+            pastSearch.append(button);
+            recentSearchesList.append(pastSearch);
         });
         noSearchesMessage.style.display = "none";
+    } else {
+        noSearchesMessage.style.display = "block";
     }
-};
-
-searchButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    handleNewSearch();
-});
+}
 
 const handleNewSearch = () => {
     const cityInput = document.querySelector("#city").value;
@@ -53,7 +50,7 @@ const renderSearchHistory = (userSearch) => {
 };
 
 const displayWeather = (userSearch) => {
-    const OpenWeatherAPIKey = "3230124ef84390459285ccaaa719d2e6";
+    const OpenWeatherAPIKey = "17a651c729295f5f4a8c171d3a2de4ea";
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${userSearch}&appid=${OpenWeatherAPIKey}&units=imperial`;
 
     fetch(url)
@@ -77,7 +74,6 @@ const displayWeather = (userSearch) => {
             const currentHumidity = document.createElement("p");
             currentHumidity.innerHTML = "Humidity: " + data.list[0].main.humidity + "%";
             currentConditions.appendChild(currentHumidity);
-
             // day 1 forecast
             const day1Conditions = document.querySelector("#day-1-forecast");
             day1Conditions.innerHTML = "";
@@ -98,78 +94,78 @@ const displayWeather = (userSearch) => {
             day1Conditions.appendChild(day1Humidity);
 
             // day 2 forecast
-const day2Conditions = document.querySelector("#day-2-forecast");
-day2Conditions.innerHTML = "";
-const day2Info = document.createElement("h3");
-day2Info.innerHTML = data.list[15].dt_txt;
-const day2Icon = document.createElement("img");
-day2Icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[15].weather[0].icon}.png`);
-day2Info.appendChild(day2Icon);
-day2Conditions.appendChild(day2Info);
-const day2Temp = document.createElement("p");
-day2Temp.innerHTML = "Temperature: " + data.list[15].main.temp + "°F";
-day2Conditions.appendChild(day2Temp);
-const day2Wind = document.createElement("p");
-day2Wind.innerHTML = "Wind speed: " + data.list[15].wind.speed + " MPH";
-day2Conditions.appendChild(day2Wind);
-const day2Humidity = document.createElement("p");
-day2Humidity.innerHTML = "Humidity: " + data.list[15].main.humidity + "%";
-day2Conditions.appendChild(day2Humidity);
+            const day2Conditions = document.querySelector("#day-2-forecast");
+            day2Conditions.innerHTML = "";
+            const day2Info = document.createElement("h3");
+            day2Info.innerHTML = data.list[15].dt_txt;
+            const day2Icon = document.createElement("img");
+            day2Icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[15].weather[0].icon}.png`);
+            day2Info.appendChild(day2Icon);
+            day2Conditions.appendChild(day2Info);
+            const day2Temp = document.createElement("p");
+            day2Temp.innerHTML = "Temperature: " + data.list[15].main.temp + "°F";
+            day2Conditions.appendChild(day2Temp);
+            const day2Wind = document.createElement("p");
+            day2Wind.innerHTML = "Wind speed: " + data.list[15].wind.speed + " MPH";
+            day2Conditions.appendChild(day2Wind);
+            const day2Humidity = document.createElement("p");
+            day2Humidity.innerHTML = "Humidity: " + data.list[15].main.humidity + "%";
+            day2Conditions.appendChild(day2Humidity);
 
-// day 3 forecast
-const day3Conditions = document.querySelector("#day-3-forecast");
-day3Conditions.innerHTML = "";
-const day3Info = document.createElement("h3");
-day3Info.innerHTML = data.list[23].dt_txt;
-const day3Icon = document.createElement("img");
-day3Icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[23].weather[0].icon}.png`);
-day3Info.appendChild(day3Icon);
-day3Conditions.appendChild(day3Info);
-const day3Temp = document.createElement("p");
-day3Temp.innerHTML = "Temperature: " + data.list[23].main.temp + "°F";
-day3Conditions.appendChild(day3Temp);
-const day3Wind = document.createElement("p");
-day3Wind.innerHTML = "Wind speed: " + data.list[23].wind.speed + " MPH";
-day3Conditions.appendChild(day3Wind);
-const day3Humidity = document.createElement("p");
-day3Humidity.innerHTML = "Humidity: " + data.list[23].main.humidity + "%";
-day3Conditions.appendChild(day3Humidity);
+            // day 3 forecast
+            const day3Conditions = document.querySelector("#day-3-forecast");
+            day3Conditions.innerHTML = "";
+            const day3Info = document.createElement("h3");
+            day3Info.innerHTML = data.list[23].dt_txt;
+            const day3Icon = document.createElement("img");
+            day3Icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[23].weather[0].icon}.png`);
+            day3Info.appendChild(day3Icon);
+            day3Conditions.appendChild(day3Info);
+            const day3Temp = document.createElement("p");
+            day3Temp.innerHTML = "Temperature: " + data.list[23].main.temp + "°F";
+            day3Conditions.appendChild(day3Temp);
+            const day3Wind = document.createElement("p");
+            day3Wind.innerHTML = "Wind speed: " + data.list[23].wind.speed + " MPH";
+            day3Conditions.appendChild(day3Wind);
+            const day3Humidity = document.createElement("p");
+            day3Humidity.innerHTML = "Humidity: " + data.list[23].main.humidity + "%";
+            day3Conditions.appendChild(day3Humidity);
 
-// day 4 forecast
-const day4Conditions = document.querySelector("#day-4-forecast");
-day4Conditions.innerHTML = "";
-const day4Info = document.createElement("h3");
-day4Info.innerHTML = data.list[31].dt_txt;
-const day4Icon = document.createElement("img");
-day4Icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[31].weather[0].icon}.png`);
-day4Info.appendChild(day4Icon);
-day4Conditions.appendChild(day4Info);
-const day4Temp = document.createElement("p");
-day4Temp.innerHTML = "Temperature: " + data.list[31].main.temp + "°F";
-day4Conditions.appendChild(day4Temp);
-const day4Wind = document.createElement("p");
-day4Wind.innerHTML = "Wind speed: " + data.list[31].wind.speed + " MPH";
-day4Conditions.appendChild(day4Wind);
-const day4Humidity = document.createElement("p");
-day4Humidity.innerHTML = "Humidity: " + data.list[31].main.humidity + "%";
-day4Conditions.appendChild(day4Humidity);
+            // day 4 forecast
+            const day4Conditions = document.querySelector("#day-4-forecast");
+            day4Conditions.innerHTML = "";
+            const day4Info = document.createElement("h3");
+            day4Info.innerHTML = data.list[31].dt_txt;
+            const day4Icon = document.createElement("img");
+            day4Icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[31].weather[0].icon}.png`);
+            day4Info.appendChild(day4Icon);
+            day4Conditions.appendChild(day4Info);
+            const day4Temp = document.createElement("p");
+            day4Temp.innerHTML = "Temperature: " + data.list[31].main.temp + "°F";
+            day4Conditions.appendChild(day4Temp);
+            const day4Wind = document.createElement("p");
+            day4Wind.innerHTML = "Wind speed: " + data.list[31].wind.speed + " MPH";
+            day4Conditions.appendChild(day4Wind);
+            const day4Humidity = document.createElement("p");
+            day4Humidity.innerHTML = "Humidity: " + data.list[31].main.humidity + "%";
+            day4Conditions.appendChild(day4Humidity);
 
-// day 5 forecast
-const day5Conditions = document.querySelector("#day-5-forecast");
-day5Conditions.innerHTML = "";
-const day5Info = document.createElement("h3");
-day5Info.innerHTML = data.list[39].dt_txt;
-const day5Icon = document.createElement("img");
-day5Icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[39].weather[0].icon}.png`);
-day5Info.appendChild(day5Icon);
-day5Conditions.appendChild(day5Info);
-const day5Temp = document.createElement("p");
-day5Temp.innerHTML = "Temperature: " + data.list[39].main.temp + "°F";
-day5Conditions.appendChild(day5Temp);
-const day5Wind = document.createElement("p");
-day5Wind.innerHTML = "Wind speed: " + data.list[39].wind.speed + " MPH";
-day5Conditions.appendChild(day5Wind);
-const day5Humidity = document.createElement("p");
-day5Humidity.innerHTML = "Humidity: " + data.list[39].main.humidity + "%";
-day5Conditions.appendChild(day5Humidity);
-})};
+            // day 5 forecast
+            const day5Conditions = document.querySelector("#day-5-forecast");
+            day5Conditions.innerHTML = "";
+            const day5Info = document.createElement("h3");
+            day5Info.innerHTML = data.list[39].dt_txt;
+            const day5Icon = document.createElement("img");
+            day5Icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[39].weather[0].icon}.png`);
+            day5Info.appendChild(day5Icon);
+            day5Conditions.appendChild(day5Info);
+            const day5Temp = document.createElement("p");
+            day5Temp.innerHTML = "Temperature: " + data.list[39].main.temp + "°F";
+            day5Conditions.appendChild(day5Temp);
+            const day5Wind = document.createElement("p");
+            day5Wind.innerHTML = "Wind speed: " + data.list[39].wind.speed + " MPH";
+            day5Conditions.appendChild(day5Wind);
+            const day5Humidity = document.createElement("p");
+            day5Humidity.innerHTML = "Humidity: " + data.list[39].main.humidity + "%";
+            day5Conditions.appendChild(day5Humidity);
+ })};
